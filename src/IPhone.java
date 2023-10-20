260734d2e088f174a18407de4c5806d5b311e2fb
Bárbara Garmendia <barbara.garmendia@hotmail.com>
2023-10-20 22:09:02 +0100

    feat: create interface and classe iphone

---
+++ src/IPhone.java
@@ -0,0 +1,33 @@
+import interfaces.AparelhoTelefonico;
+import interfaces.NavegadorInternet;
+import interfaces.ReprodutorMusical;
+
+public class IPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+    
+    public void tocar() {
+    }
+    
+    public void pausar() {
+    }
+
+    public void selecionarMusica() {
+    }
+    
+    public void ligar() {
+    }
+
+    public void atender() {
+    }
+    
+    public void iniciarCorreioVoz() {
+    }
+
+    public void exibirPagina() {
+    }
+
+    public void adicionarNovaAba() {
+    }
+
+    public void atualizarPagina() {
+    }
+}
